aedffd777825bc398cd50679dc7b32a7c46fde86
satria-ui <dimbum14@gmail.com>
2023-02-07 21:06:41 +0700

    feat: gridsearch

--- script/Code/svm.py
+++ script/Code/svm.py
@@ -1,7 +1,5 @@
-from utils import audio_extraction, data_loader
+from utils import audio_extraction
 from sklearn.preprocessing import LabelEncoder
-import pandas as pd
-import numpy as np
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn import svm
@@ -29,33 +27,38 @@ def main():
     x_train = scaler.fit_transform(X_train)
     x_test = scaler.transform(X_test)
 
-    # # GridSearching
-    # # param_grid = {'C': [0.1,1, 10, 100], 'gamma': [1,0.1,0.01,0.001],'kernel': ['rbf', 'poly', 'sigmoid']}
+    # GridSearching
+    param_grid = {'C': [0.1,1,10], 'gamma': [1,0.1,0.01,0.001,'scale', 'auto'],'kernel': ['linear', 'rbf', 'poly', 'sigmoid'], 'probability':[True, False]}
 
-    # # grid = GridSearchCV(svm.SVC(),param_grid,refit=True,verbose=2)
-    # # grid.fit(x_train,y_train)
+    grid = GridSearchCV(svm.SVC(),param_grid,refit=True,verbose=2)
+    grid.fit(x_train,y_train)
+    
+    print(" Results from Grid Search " )
+    print("\n The best estimator across ALL searched params:\n",grid.best_estimator_)
+    print("\n The best score across ALL searched params:\n",grid.best_score_)
+    print("\n The best parameters across ALL searched params:\n",grid.best_params_)
 
-    print("Training...")
-    SVM_model = svm.SVC(kernel='linear')
-    SVM_model.fit(x_train, y_train)
-    print("Saving Model...")
-    filename = '../ML_Model/SVM_Model.sav'
-    pickle.dump(SVM_model, open(filename, 'wb'))
-    print("Done.")
+    # print("Training...")
+    # SVM_model = svm.SVC(kernel='linear')
+    # SVM_model.fit(x_train, y_train)
+    # print("Saving Model...")
+    # filename = '../ML_Model/SVM_Model.sav'
+    # pickle.dump(SVM_model, open(filename, 'wb'))
+    # print("Done.")
 
-    y_pred=SVM_model.predict(x_test)
+    # y_pred=SVM_model.predict(x_test)
 
-    report=classification_report(y_test, y_pred)
-    accuracy=accuracy_score(y_true=y_test, y_pred=y_pred)
+    # report=classification_report(y_test, y_pred)
+    # accuracy=accuracy_score(y_true=y_test, y_pred=y_pred)
 
-    print("Model Summary:\n")
-    print("Model:{}    Accuracy: {:.2f}%".format(type(SVM_model).__name__ , accuracy*100))
-    print(report)
+    # print("Model Summary:\n")
+    # print("Model:{}    Accuracy: {:.2f}%".format(type(SVM_model).__name__ , accuracy*100))
+    # print(report)
 
-    print("The Model's Prediction ")
-    print("<<<===========================================>>>")
-    df = pd.DataFrame({'Actual': y_test, 'Predict': y_pred})
-    print(df.head(10))
+    # print("The Model's Prediction ")
+    # print("<<<===========================================>>>")
+    # df = pd.DataFrame({'Actual': y_test, 'Predict': y_pred})
+    # print(df.head(10))
 
 if __name__ == '__main__':
     main()
